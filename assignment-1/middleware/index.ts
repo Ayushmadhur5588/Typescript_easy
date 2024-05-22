d937f939d7254b4495c67f9e9f40ad124be14e81
Ayushmadhur5588 <cuteboy.bhardwaj@gmail.com>
2024-05-22 20:53:17 +0530

    Moving to TS

--- assignment-1/middleware/index.ts
+++ assignment-1/middleware/index.ts
@@ -1,15 +1,26 @@
 import jwt from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
 
-export const authenticateJwt = (req, res, next) => {
+export const authenticateJwt = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const authHeader = req.headers.authorization;
   if (authHeader) {
     const token = authHeader.split(" ")[1];
-    jwt.verify(token, process.env.SECRET, (err, user) => {
+    jwt.verify(token, process.env.SECRET, (err, payload) => {
       if (err) {
         return res.sendStatus(403);
       }
-      console.log(user);
-      req.userId = user.id;
+      if (!payload) {
+        return res.sendStatus(403);
+      }
+      if (typeof payload === "string") {
+        return res.sendStatus(403);
+      }
+
+      req.headers["userId"] = payload.id;
       next();
     });
   } else {
